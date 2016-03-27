a28671e50bcdf61611a85b4f137344bd699f9ca6
Benjamin Scabbia <benscabbia@gmail.com>
2016-03-27 17:29:47 +0100

    first commit

---
+++ MClient/src/main/java/MClient/MClientApplication.java
@@ -0,0 +1,12 @@
+package MClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MClientApplication.class, args);
+	}
+}
