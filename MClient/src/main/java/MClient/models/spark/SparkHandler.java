95b4d2d9d6f64448fa9baa2a7a883db0e3eb81dd
Benjamin Scabbia <benscabbia@gmail.com>
2016-05-17 22:10:57 +0100

    -server sent spark instructions are now utilised over defaults

--- MClient/src/main/java/MClient/models/spark/SparkHandler.java
+++ MClient/src/main/java/MClient/models/spark/SparkHandler.java
@@ -10,6 +10,9 @@ import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,37 +31,21 @@ public class SparkHandler {
         //d:\Ben\Desktop\test-small.txt
 
         //String t = "d:\\Ben\\Desktop\\test-small.txt";
-        String t2 = "d:/Ben/Desktop/test-small.txt";
+        //String t2 = "d:/Ben/Desktop/test-small.txt";
        // d:\Ben\Desktop
 
-
-        if(validateFile(t2)){
+        if(validateFile(filePath)){
             if(type == SparkType.WORDCOUNT){
-                return wordCount(t2, sorted);
+                return wordCount(filePath, sorted);
             }
             else if(type == SparkType.WORDSEARCH){
-                wordSearch(t2, sorted);
+                return wordSearch(filePath, sorted);
             }else{
-
+                return "Unknown Spark Command";
             }
         }else{
             return "The file could not be found or could not be read at directory: " + filePath;
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        return "";
     }
 
     private static String wordCount(String filePath, boolean sorted){
@@ -68,12 +55,28 @@ public class SparkHandler {
             // Define a configuration to use to interact with Spark
             System.out.println("==========================PHASE 1 ================");
 
-            SparkConf conf = new SparkConf().setMaster("local").setAppName("Work Count App");
+//            Random random = new Random();
+//            int max = 60000, min=3000;
+//            //SparkConf conf = new SparkConf().setMaster("local").setAppName("Work Count App" + n);
+//            while(true){
+//                int  n = random.nextInt(max - min + 1) + min;
+//                try{
+//                    conf = new SparkConf().setMaster("local").setAppName("Work Count App" + n);
+//
+//
+//                }catch (BindException ex){
+//
+//                }
+//            }
+             SparkConf conf = new SparkConf().setMaster("local").setAppName("Work Count App");
+
+            //SparkConf conf = new SparkConf().setMaster("spark://local:" + n).setAppName("Work Count App" + n);
             log += "Phase one complete\n";
 
 //
             System.out.println("==========================PHASE 2 ================");
 //        // Create a Java version of the Spark Context from the configuration
+           // JavaSparkContext sc = new JavaSparkContext(conf);
             JavaSparkContext sc = new JavaSparkContext(conf);
             log += "Phase two complete\n";
 //
@@ -120,12 +123,15 @@ public class SparkHandler {
 //        // Save the word count back out to a text file, causing evaluation.
 //
 //
+            //TODO need to check if sorted, and if so, sort the results
+            String resultSet = "";
             for(Object a : arr){
                 Tuple2<String, Integer> tuple = (Tuple2)a;
                 System.out.println(tuple);
+                resultSet += tuple + ", ";
             }
 
-            return "done";
+            return resultSet;
 
         }catch (Exception e){
             System.out.println("ERROR FROM WITHIN SPARK ");
@@ -134,7 +140,7 @@ public class SparkHandler {
     };
 
 
-    private static void wordSearch(String filePath, boolean sorted){};
+    private static String wordSearch(String filePath, boolean sorted){return "TODO implementation";};
 
     private static boolean validateFile(String filePath){
 
@@ -144,5 +150,40 @@ public class SparkHandler {
 //        System.out.println(file.exists()); //bool
 //        System.out.println(file.getParentFile()); //d:\Ben\Desktop
 //        System.out.println(file.getAbsoluteFile()); //d:\Ben\Desktop\test-small.txt
+    }
+
+
+
+    public static boolean validatePort(int port) {
+        if (port < 2000 || port > 50000) {
+            return false;
+        }
+        //implementation from Apache camel project
+        //http://svn.apache.org/viewvc/camel/trunk/components/camel-test/src/main/java/org/apache/camel/test/AvailablePortFinder.java?view=markup#l130
+
+        ServerSocket ss = null;
+        DatagramSocket ds = null;
+        try {
+            ss = new ServerSocket(port);
+            ss.setReuseAddress(true);
+            ds = new DatagramSocket(port);
+            ds.setReuseAddress(true);
+            return true;
+        } catch (IOException e) {
+        } finally {
+            if (ds != null) {
+                ds.close();
+            }
+
+            if (ss != null) {
+                try {
+                    ss.close();
+                } catch (IOException e) {
+                /* should not be thrown */
+                }
+            }
+        }
+
+        return false;
     }
 }
